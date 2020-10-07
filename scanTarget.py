c8e1968483b0af8c454896882c7ded1d1f1ea0ed
Nicholai <nickbenk@gmail.com>
2020-10-07 13:03:59 -0500

    improved

--- scanTarget.py
+++ scanTarget.py
@@ -7,7 +7,7 @@ import argparse
 from socket import *
 from threading import *
 
-semLock = Semaphore(value = 1)
+semLock = Semaphore()
 
 def connScan(tgtHost, tgtPort):
     try:
@@ -17,32 +17,33 @@ def connScan(tgtHost, tgtPort):
         s.send('root\r\n')
         data = s.recv(4096)
         semLock.acquire()
-        print("port %d is open on target", tgtPort)
+        print("port", tgtPort,  "is open on target")
         print(data)
+        s.close()
     except:
         semLock.acquire()
-        print("port %d is filtered or closed", tgtPort)
+        print("port", tgtPort,  "is filtered or closed")
     finally:
         semLock.release()
-        s.close()
+        
 
 def portScan(tgtHost, tgtPorts):
     try:
         tgtIP = gethostbyname(tgtHost)
         print(tgtIP)
     except: 
-        print("[-] Cannot resolve %s: Unknown Host", tgtHost)
+        print("[-] Cannot resolve", tgtHost, ": Unknown Host")
         return
 
     for tgtPort in tgtPorts:
-        t = Thread(target="connScan", args=(tgtHost,tgtPort))
+        t = Thread(target= connScan, args=(tgtHost, tgtPort))
         t.start()
 
-    #set timeout
+    print("finished scan")
 
 def main():
-    parser = argparse.Argument()
-    parser.add_argument('-H', dest='tgtHost', require=True, help='specify target host')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H', dest='tgtHost', required=True, help='specify target host')
     parser.add_argument('-p', dest='tgtPorts', default='21, 22, 23, 25, 80, 443', help='specify target port(s)')
      
     args = parser.parse_args()
